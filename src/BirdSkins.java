451a4ae5d925630442c13e1d86aabe57cb9d75f5
Allen Joseph <aj03borromeo@gmail.com>
2026-08-07 06:52:37 +0800

    Update BirdSkins.java

--- src/BirdSkins.java
+++ src/BirdSkins.java
@@ -1,2 +1,19 @@
 public class BirdSkins {
+    public final String name; //This is shown in the settings dropdown e.g "AllenBird"
+    public final String imagePath; //e.g "./AllenBird.png"
+    public final String jumpSoundPath; //e.g "./AllenBird.wav"
+
+    public BirdSkins(String name, String imagePath, String jumpSoundPath) {
+        this.name = name;
+        this.imagePath = imagePath;
+        this.jumpSoundPath = jumpSoundPath;
+    }
+
+    //Overriding this so the name won't show ugly.
+    //JComboBox is used in the SettingMenu class to display whatever
+    //toString() returns for each item.
+    @Override
+    public String toString() {
+        return name;
+    }
 }
